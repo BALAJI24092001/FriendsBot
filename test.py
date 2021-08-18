38226369d46931f2e21a4d4fb315deaec913edfe
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-08-18 10:21:02 +0530

    11

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-url = 'https://api.telegram.org/bot1966604778:AAFaH4Y3jmupxYGt324_1Uf6xRfy4ZseUvo/'
+url = 'https://api.telegram.org/bot1974553065:AAGm9ZRmLymL9bWx-inSxEIKLTigQHGgtT4/'
 
 
 def send_message(url, chat_id, message):
@@ -11,7 +11,28 @@ def send_message(url, chat_id, message):
         # return send
     elif message == '/start':
         reply = '''
-        hello this is a telegram bot to download friends sitcom enter the required video in this format S**E** (ex: S02E21)
+        Hey there,
+        I know you started me(the bot) probably to download
+        the F.R.I.E.N.D.S series, but before you carray on
+        with downloading, let me just give you an overview
+        of what you can do with me.
+
+        To download use the command `S01E01` to get first
+        episode of season 1.
+
+        To download a full season use `s02` (season 2).
+
+        To know number of episodes in each season use
+        `episodes` command.
+
+        To get details of the main charecters use their
+        first name as the command. eg.. (chandler)
+
+        For any suggestions or complaints to report contact
+        @dbalajivaraprasad
+        @CHAKRADHAR_GBG
+        @abhiramavala
+
         '''
         send = requests.post(url+'sendMessage?chat_id=' +
                              str(chat_id)+'&text='+reply)
