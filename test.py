4032f85b70bb194441b449c4e2cfc396c25d79ac
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-08-19 00:02:21 +0530

    message

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ dict5 = {'s05e01': 'https://t.me/friends2021complete/1327', 's05e02': 'https://t
          's05e13': 'https://t.me/friends2021complete/1339', 's01e14': 'https://t.me/friends2021complete/1340', 's05e15': 'https://t.me/friends2021complete/1341', 's05e16': 'https://t.me/friends2021complete/1342', 's05e17': 'https://t.me/friends2021complete/1343', 's05e18': 'https://t.me/friends2021complete/1344', 's05e19': 'https://t.me/friends2021complete/1345', 's05e20': 'https://t.me/friends2021complete/1346', 's05e21': 'https://t.me/friends2021complete/1347', 's05e22': 'https://t.me/friends2021complete/1348', 's05e23': 'https://t.me/friends2021complete/1349', 's05e24': 'https://t.me/friends2021complete/1350'}
 
 dict6 = {'s06e01': 'https://t.me/friends2021complete/1352', 's06e02': 'https://t.me/friends2021complete/1355', 's06e03': 'https://t.me/friends2021complete/1356', 's06e04': 'https://t.me/friends2021complete/1357', 's06e05': 'https://t.me/friends2021complete/1358', 's06e06': 'https://t.me/friends2021complete/1359', 's06e07': 'https://t.me/friends2021complete/1360', 's06e08': 'https://t.me/friends2021complete/1364', 's06e09': 'https://t.me/friends2021complete/1366', 's06e10': 'https://t.me/friends2021complete/1369', 's06e11': 'https://t.me/friends2021complete/1370', 's06e12': 'https://t.me/friends2021complete/1371',
-         's06e13': 'https://t.me/friends2021complete/1372', 's01e14': 'https://t.me/friends2021complete/1373', 's06e15': 'https://t.me/friends2021complete/1374', 's06e16': 'https://t.me/friends2021complete/1375', 's06e17': 'https://t.me/friends2021complete/1376', 's06e18': 'https://t.me/friends2021complete/1378', 's06e19': 'https://t.me/friends2021complete/1380', 's06e20': 'https://t.me/friends2021complete/1383', 's06e21': 'https://t.me/friends2021complete/1385', 's06e22': 'https://t.me/friends2021complete/1390', 's06e23': 'https://t.me/friends2021complete/1391', 's06e24': 'https://t.me/friends2021complete/1392','s06s25':'https://t.me/friends2021complete/1393'}
+         's06e13': 'https://t.me/friends2021complete/1372', 's01e14': 'https://t.me/friends2021complete/1373', 's06e15': 'https://t.me/friends2021complete/1374', 's06e16': 'https://t.me/friends2021complete/1375', 's06e17': 'https://t.me/friends2021complete/1376', 's06e18': 'https://t.me/friends2021complete/1378', 's06e19': 'https://t.me/friends2021complete/1380', 's06e20': 'https://t.me/friends2021complete/1383', 's06e21': 'https://t.me/friends2021complete/1385', 's06e22': 'https://t.me/friends2021complete/1390', 's06e23': 'https://t.me/friends2021complete/1391', 's06e24': 'https://t.me/friends2021complete/1392', 's06s25': 'https://t.me/friends2021complete/1393'}
 
 dict7 = {'s07e01': '', 's07e02': '', 's07e03': '', 's07e04': '', 's07e05': '', 's07e06': '', 's07e07': '', 's07e08': '', 's07e09': '', 's07e10': '', 's07e11': '', 's07e12': '',
          's07e13': '', 's01e14': '', 's07e15': '', 's07e16': '', 's07e17': '', 's07e18': '', 's07e19': '', 's07e20': '', 's07e21': '', 's07e22': '', 's07e23': '', 's07e24': ''}
@@ -69,30 +69,54 @@ def send_message(url, chat_id, message):
                              str(chat_id)+'&text='+reply)
         print(send)
         # return send
-        
-#start 
-    elif message =='/start':
-        reply ='''
+
+# start
+    elif message == '/start':
+        reply = '''
         Hola! 
-        I'm the F.R.I.E.N.D.S bot, here to help you with downloing the series.
+        I'm the F.R.I.E.N.D.S bot, here 
+        to help you with downloing the 
+        series.
 
-                i.      Use `s01e01` to download episode 1 of season 1.
-                ii.     Use `s05` to download season 5 all episodes.
-                iii.    If you want to know more about the series and some interesting facts about our main charecters,use their first name as the command.
-                iv.     For any help regarding the commands, use the command `help` or `/help`. 
-                4.And lastly to know about the developers who made me type info)
-                '''
-        send = requests.post(url+'sendMessage?chat_id=' + str(chat_id)+'&text='+reply)
+        Use `s01e01` to download episode 
+        1 of season 1.
 
-#info
+        Use `s05` to download season 5 
+        all episodes.
+
+        If you want to know more about 
+        the series and some interesting 
+        facts about our main charecters, 
+        use their first name as the 
+        command.
+
+        For any help regarding the 
+        commands, use the command 
+        `help` or `/help`. 
+
+        To get to know about the 
+        developers use the command 
+        `info`.
+
+        PS: The episode and season 
+        commands give you two ways to 
+        access the series. One, direcly 
+        download the episode another to 
+        stream it online using "playit" 
+        app in your mobiles.
+        '''
+        send = requests.post(url+'sendMessage?chat_id=' +
+                             str(chat_id)+'&text='+reply)
+
+# info
     elif message == 'info':
         reply = '''
-       Hey there! we are one of the biggest fans of the sitcom F.R.I.E.N.D.S so we wanted to make this available for all people so they can watch like us and enjoy
+       Hey there! 
+       We are the biggest fans of the sitcom F.R.I.E.N.D.S, so we wanted to make this available for all people so they can access it easily.
         For any suggestions or complaints to report contact
         @dbalajivaraprasad
         @CHAKRADHAR_GBG
         @abhiramavala
-
         '''
         send = requests.post(url+'sendMessage?chat_id=' +
                              str(chat_id)+'&text='+reply)
