6991b82fe72dfdecdc816a5cfced70e27a09b2c9
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2021-08-23 00:19:57 +0530

    custom message

---
+++ customReplyTest.py
@@ -0,0 +1,5 @@
+
+import requests
+url = 'https://api.telegram.org/bot/'
+requests.post(url+'sendMessage?chat_id=' +
+              str(943836998)+'&text='+'inko sari nee prend ni backstab lu annavu ante nee number black market lo petti bengutha mowa')
