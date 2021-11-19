fa963c8d81fda5fd009fb83816b369aa399e3e2b
BALAJI VARA PRASAD <dbalajivaraprasad@gmail.com>
2021-11-19 21:51:00 +0530

    Delete customReplyTest.py

--- customReplyTest.py
+++
@@ -1,5 +0,0 @@
-
-import requests
-url = 'https://api.telegram.org/bot/'
-requests.post(url+'sendMessage?chat_id=' +
-              str(943836998)+'&text='+'inko sari nee prend ni backstab lu annavu ante nee number black market lo petti bengutha mowa')
